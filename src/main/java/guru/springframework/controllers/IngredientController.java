4e3b8da08837187cda0d92a4a18815cbe71ade79
Błażej <blazejknie@gmail.com>
2022-02-09 09:28:02 +0100

    Add view of recipe ingredients

---
+++ src/main/java/guru/springframework/controllers/IngredientController.java
@@ -0,0 +1,26 @@
+package guru.springframework.controllers;
+
+import guru.springframework.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+@Slf4j
+@Controller
+public class IngredientController {
+
+    private final RecipeService recipeService;
+
+    public IngredientController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+    @GetMapping("/recipe/{recipeId}/ingredients")
+    public String listIngredients(@PathVariable String recipeId, Model model) {
+        log.debug("Adding ingredients to Model for Recipe Id: " + recipeId);
+
+        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(recipeId)));
+
+        return "/recipe/ingredient/list";
+    }
+}
